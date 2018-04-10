edc41e1b156c7765b6bee8bfb6d85503ca222af9
Oreo <liuyuanlin@156.com>
2018-04-10 22:14:20 +0800

    add binary complete

--- src/main/java/code/lintcode/binary/AddBinary.java
+++ src/main/java/code/lintcode/binary/AddBinary.java
@@ -2,15 +2,24 @@ package code.lintcode.binary;
 
 public class AddBinary {
 	public static void main(String[] args) {
-		char a = '1';
-		char b = '1';
-		System.out.println(((a - '0') + (b - '0')));
-		System.out.println('0' - '1');
+		System.out.println('1' & '1');
+		System.out.println('1' & '0');
+		System.out.println('0' & '0');
+		System.out.println('1' ^ '1');
+		System.out.println('1' ^ '0');
+		System.out.println('0' ^ '0');
+		System.out.println((char) 49);
 	}
 
 	public String addBinary(String a, String b) {
 		StringBuilder s = new StringBuilder();
-
-		return null;
+		int i = a.length() - 1, j = b.length() - 1, c = 0, p = 0;
+		while (i >= 0 || j >= 0 || c == 1) {
+			c += i >= 0 ? a.charAt(i--) - '0' : 0;
+			c += j >= 0 ? b.charAt(j--) - '0' : 0;
+			s.append(c % 2);
+			c /= 2;
+		}
+		return s.reverse().toString();
 	}
 }
