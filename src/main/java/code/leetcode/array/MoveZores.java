8267a715caddbe240427aad37a929c6635b54303
Oreo <liuyuanlin@156.com>
2018-04-11 22:15:12 +0800

    continue practice on leetcode array

--- src/main/java/code/leetcode/array/MoveZores.java
+++ src/main/java/code/leetcode/array/MoveZores.java
@@ -2,7 +2,25 @@ package code.leetcode.array;
 
 public class MoveZores {
 
-	public void moveZeroes(int[] nums) {
+	public static void main(String[] args) {
+		int[] nums = { 0, 1, 0, 3, 12 };
+		moveZeroes(nums);
+	}
+
+	public static void moveZeroes(int[] nums) {
+		int zi = -1;
+		for (int i = 0; i < nums.length; i++) {
+			if (nums[i] == 0) {
+				if (zi == -1)
+					zi = i;
+			} else {
+				if (zi != -1) {
+					nums[i] = nums[i] ^ nums[zi];
+					nums[zi] = nums[i] ^ nums[zi];
+					nums[i] = nums[i] ^ nums[zi++];
+				}
+			}
+		}
 	}
 
 }
---
+++ src/main/java/code/leetcode/array/TwoSum.java
@@ -0,0 +1,28 @@
+package code.leetcode.array;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class TwoSum {
+
+	public static void main(String[] args) {
+		int[] nums = { 3, 2, 4 };
+		int target = 6;
+		twoSum(nums, target);
+	}
+
+	public static int[] twoSum(int[] nums, int target) {
+		Map<Integer, Integer> s = new HashMap<Integer, Integer>();
+		int[] r = new int[2];
+		for (int i = 0; i < nums.length; i++) {
+			int t = target - nums[i];
+			if (s.containsKey(nums[i])) {
+				r[0] = s.get(nums[i]);
+				r[1] = i;
+				break;
+			}
+			s.put(t, i);
+		}
+		return r;
+	}
+}
