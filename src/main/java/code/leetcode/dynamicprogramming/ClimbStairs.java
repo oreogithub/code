c136c9023122920e1cc07b67a963ff24b7aa3740
Oreo <liuyuanlin156@qq.com>
2018-04-20 16:31:17 +0800

    complete tree and sortedsearch and started dynamic programming

---
+++ src/main/java/code/leetcode/dynamicprogramming/ClimbStairs.java
@@ -0,0 +1,25 @@
+package code.leetcode.dynamicprogramming;
+
+public class ClimbStairs {
+	/**
+	 * exceed time limits
+	 * 
+	 * @param n
+	 * @return
+	 */
+	public int climbStairs(int n) {
+		if (n < 3)
+			return n;
+		return climbStairs(n - 1) + climbStairs(n - 2);
+	}
+
+	public int climbStairs2(int n) {
+		int step1 = 1, step2 = 1;
+		for (int i = 1; i < n; i++) {
+			int temp = step2;
+			step2 = step1;
+			step1 = step1 + temp;
+		}
+		return step1;
+	}
+}
