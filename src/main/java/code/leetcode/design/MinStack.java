54e6e9c217031496dcfd192e1daebc5d17ac876f
Oreo <liuyuanlin156@qq.com>
2018-04-24 18:37:16 +0800

    commit min statck

--- src/main/java/code/leetcode/design/MinStack.java
+++ src/main/java/code/leetcode/design/MinStack.java
@@ -2,6 +2,9 @@ package code.leetcode.design;
 
 public class MinStack {
 	/** initialize your data structure here. */
+	private NodeWithMin[] stack = new NodeWithMin[10];
+	private int head = -1, tail = 0;
+
 	public MinStack() {
 
 	}
@@ -10,8 +13,49 @@ public class MinStack {
 		this.push(new Node(x));
 	}
 
+	public void pop() {
+		if (tail > head)
+			return;
+		NodeWithMin node = stack[head];
+		stack[head--] = null;
+		// node.minNode = null;
+		// return node.node;
+	}
+
 	public int top() {
-		return this.pop().val;
+		if (tail > head)
+			return -1;
+		NodeWithMin node = stack[head];
+		stack[head--] = null;
+		// node.minNode = null;
+		return node.node.val;
+	}
+
+	public int getMin() {
+		Node minNode = this.getMinNode();
+		if (minNode == null)
+			return -1;
+		return minNode.val;
+	}
+
+	private Node getMinNode() {
+		if (tail > head)
+			return null;
+		NodeWithMin node = stack[head];
+		if (node == null)
+			return null;
+		return node.minNode;
+	}
+
+	private void push(Node node) {
+		if (node == null)
+			return;
+		Node minNode = this.getMinNode();
+		if (minNode == null)
+			minNode = node;
+		else
+			minNode = minNode.val > node.val ? node : minNode;
+		stack[++head] = new NodeWithMin(node, minNode);
 	}
 
 	static class Node {
@@ -31,41 +75,4 @@ public class MinStack {
 			this.minNode = minNode;
 		}
 	}
-
-	private NodeWithMin[] stack = new NodeWithMin[10];
-	private int head = -1, tail = 0;
-
-	private Node pop() {
-		if (tail > head)
-			return null;
-		NodeWithMin node = stack[head];
-		stack[head--] = null;
-		// node.minNode = null;
-		return node.node;
-	}
-
-	public Node peak() {
-		if (tail > head)
-			return null;
-		return stack[head].node;
-	}
-
-	private void push(Node node) {
-		if (node == null)
-			return;
-		Node minNode = this.getMin();
-		if (minNode == null)
-			minNode = node;
-		stack[++head] = new NodeWithMin(node, minNode);
-	}
-
-	public Node getMin() {
-		if (tail > head)
-			return null;
-		NodeWithMin node = stack[head];
-		if (node == null)
-			return null;
-		return node.minNode;
-	}
-
 }
